007485727a36cc05aeafeb4b45f1b20c062bbc10
joao_davi <sdavidmoura3@gmail.com>
2023-10-26 22:46:56 -0300

    Adicionando os projetos de peso 2

---
+++ Scripts/projetos_peso2/pula_pula_parquinho/test_crianca.py
@@ -0,0 +1,17 @@
+import unittest
+
+from POO_projects.Scripts.projetos_peso2.pula_pula_parquinho.crianca import Crianca
+
+
+class CriancaTest(unittest.TestCase):
+
+    def testInicializandoCrianca(self):
+        crianca = Crianca("Dora", 6)
+        self.assertEqual(6, crianca.getIdade(),
+                     "Ao inicializar uma crianca ela deve ter a mesma idade que foi passada na inicialização")
+        self.assertEqual("Dora", crianca.getNome(),
+                     "Ao inicializar uma crianca ela deve ter o mesmo nome que foi passada na inicialização")
+
+
+if __name__ == '__main__':
+    unittest.main()
